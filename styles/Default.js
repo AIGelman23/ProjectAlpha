58d2ce1cec8c269478196aae440b0e68c7ba293a
Ariel Gelman <AIGelman23@gmail.com>
2022-12-12 00:17:21 -0500

    - Removed Colors.js file temporarily - Added color of black to background-color of screen Wrapper component

---
+++ styles/Default.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.View`
+    display: 'flex';
+    align-items: 'center';
+    justify-content: 'center';
+    flex: 1;
+    padding: 50px;
+    background-color: black;
+`;
+
