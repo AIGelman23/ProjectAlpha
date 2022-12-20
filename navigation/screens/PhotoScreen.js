4af53e76c53b281be793075d4542c2f9ceac2579
Ariel Gelman <AIGelman23@gmail.com>
2022-12-19 23:17:59 -0500

    Added PhotoScreen.js, SettingsScreen.js

---
+++ navigation/screens/PhotoScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text, Pressable, SafeAreaView, StyleSheet } from "react-native";
+import { Wrapper } from "../../styles/Default";
+
+export const PhotoScreen = ({ route, navigation }) => {
+  return <Wrapper></Wrapper>;
+};
