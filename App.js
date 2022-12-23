5e83249d24a66fcfc2e5e495e9fa2789991d9da7
Ariel Gelman <AIGelman23@gmail.com>
2022-12-22 22:05:46 -0500

    Updated App.js to be root of where custom fonts are loaded

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
-import React from "react";
-import AppNavigator from "./navigation/AppNavigator";
+import React, { useCallback} from "react";
+import { useFonts } from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { AuthStack } from "./navigation/AuthStack";
+import { AppStack } from "./navigation/AppStack";
 
 export default function App() {
 
+  const [fontsLoaded] = useFonts({
+    'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+  });
+
+  useCallback(async () => {
+    if (fontsLoaded) {
+      console.log("fontsLoaded:");
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-      <AppNavigator />
+    <NavigationContainer>
+      {/* <AppStack /> */}
+      <AuthStack />
+    </NavigationContainer>
   );
 }
