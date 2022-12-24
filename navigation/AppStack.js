a82afc4bc2d64d65cbaf5766b11883a9aadaff68
Ariel Gelman <AIGelman23@gmail.com>
2022-12-23 23:28:47 -0500

    - Removed NavigationContainer surrounding Stack.Screen in AppStack.js - Removed NavigationContainer import

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,14 +1,13 @@
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { TabNavigator } from "./TabNavigator";
 import { BottomTabBarHeightContext } from "@react-navigation/bottom-tabs";
 
 const Stack = createNativeStackNavigator();
 
-export default AppNavigator = () => {
+export const AppStack = () => {
+  
   return (
-    <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
           name="Main"
@@ -16,6 +15,5 @@ export default AppNavigator = () => {
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
-    </NavigationContainer>
   );
 };
