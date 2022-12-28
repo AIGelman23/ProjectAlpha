27a5ad1cbf6ac1ec730767dce904ecad5be0fff7
Ariel Gelman <AIGelman23@gmail.com>
2022-12-27 22:04:00 -0500

    Updated LoginButton with imported styled-component AuthButton and AuthButtonLabel

--- components/buttons/LoginButton.js
+++ components/buttons/LoginButton.js
@@ -1,27 +1,13 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { AuthButton, AuthButtonLabel } from "../../styles/GlobalStyle";
 
 export const LoginButton = ({ label, onPress }) => {
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={{
-        backgroundColor: "blue",
-        padding: 15,
-        borderRadius: 20,
-        marginBottom: 25,
-      }}
-    >
-      <Text
-        style={{
-          textAlign: "center",
-          fontWeight: "700",
-          fontSize: 16,
-          color: "#fff",
-        }}
-      >
+    <AuthButton
+      onPress={onPress}>
+      <AuthButtonLabel>
         {label}
-      </Text>
-    </TouchableOpacity>
+      </AuthButtonLabel>
+    </AuthButton>
   );
 };
